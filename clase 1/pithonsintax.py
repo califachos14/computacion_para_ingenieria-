d8b367887bfa175914ad0f2ef3101dbebaa1740b
Your Name <you@example.com>
2022-02-01 12:56:53 +0100

    modificacion clase 1

---
+++ clase 1/pithonsintax.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 12:20:25 2022
+
+@author: Diego Lima
+"""
+
+# creae una constante 
+Name = "amibael"
+FULL_NAME = "santiago lima zambrana"
+print (Name)
+print (FULL_NAME)
+
+#tipos de datos
+entero = 90
+print (f"el entero tienen valor de {entero}")
+decimal = 6.14
+print (f"el entero tienen valor de {decimal}")
+caracter = "A"
+print (f"el entero tienen valor de {caracter}")
+cadena = "primera cadena"
+print (f"el entero tienen valor de {cadena}")
+cadena_2 = "segunda cadena"
+print (f"el entero tienen valor de {cadena_2}")
+booleano = False #valores true o false
+print (f"el valor de la decimal es {booleano}")
+#LISTA
+list= [1,2,3,4]
+print (f"el valor de la lista es : {list}")
+# diccionario
+
+diccionario = {'nombre' : 'amibael lima', "edad" : 30}
+nombre = diccionario ["nombre"]
+edad = diccionario ["edad"]
+print (f'el nombre es : {nombre}y su edad {edad}')
+#ENTRADAS ESTANDAR 
+telefono = input ("ingrese el numero telefonico")
+#cast de tipos
+a = int(input ("Ingrease a :"))
+b = int(input ("Ingrease b :"))
+
+print(f'resultado de sumar a + b = {a+b}')
+
+## Estructuras de control
